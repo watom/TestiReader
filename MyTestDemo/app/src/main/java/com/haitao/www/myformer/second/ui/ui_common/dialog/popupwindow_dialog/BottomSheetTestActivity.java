b82f138b9c2266b987736220f0646c0983a4fa8e
wanghaitao <15339167593@163.com>
2018-11-27 18:36:03 +0800

    开发表情键盘

--- MyTestDemo/app/src/main/java/com/haitao/www/myformer/second/ui/ui_common/dialog/popupwindow_dialog/BottomSheetTestActivity.java
+++ MyTestDemo/app/src/main/java/com/haitao/www/myformer/second/ui/ui_common/dialog/popupwindow_dialog/BottomSheetTestActivity.java
@@ -1,5 +1,6 @@
 package com.haitao.www.myformer.second.ui.ui_common.dialog.popupwindow_dialog;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -26,8 +27,7 @@ public class BottomSheetTestActivity extends AppCompatActivity implements View.O
     private Button btnSougoKeyboard;
     private Button btnSecurityKeyboard;
     private TextView tvEmojiSample;
-    private LinearLayout roundPointGroup;
-    private int lastPosition;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,20 +53,21 @@ public class BottomSheetTestActivity extends AppCompatActivity implements View.O
         //把这个底部菜单和一个BottomSheetBehavior关联起来
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
 
-        int unicodeJoy = 0x1F602;
+        int unicodeJoy = 0x1F639;
         String emojiString = getEmojiStringByUnicode(unicodeJoy);
         tvEmojiSample.setText(emojiString);
     }
 
     /**
      * 功能：将emoji表情显示到页面上
-     *
+     * <p>
      * 原理：通过emoji表情的通用Unicode编码就可以实现，直接使用Character.toChars()方法将unicode编码转换为一个char数组，
      * 再将这个char数组转换成为字符串就可以直接操作了，系统会自动将其解析为表情图片，可以直接显示在textview组件当中，不需要我们做任何其他的事情。
+     *
      * @param unicode emoji表情对应的unicode编码
      * @return
      */
-    private String getEmojiStringByUnicode(int unicode){
+    private String getEmojiStringByUnicode(int unicode) {
         return new String(Character.toChars(unicode));
     }
 
@@ -108,47 +109,25 @@ public class BottomSheetTestActivity extends AppCompatActivity implements View.O
     private void wechatKeyboard() {
         TabLayout keyboardTabLayout = findViewById(R.id.keyboard_tab_layout);
         ViewPager keyboardContainerViewpager = findViewById(R.id.keyboard_container_viewpager);
-        roundPointGroup = findViewById(R.id.dot_viewgroup);
+        LinearLayout roundPointGroup = findViewById(R.id.dot_viewgroup);
 
         KeyboardFragmentAdapter keyboardFragmentAdapter = new KeyboardFragmentAdapter(getSupportFragmentManager(), this);
         keyboardContainerViewpager.setAdapter(keyboardFragmentAdapter);
         //绑定指示器与ViewPager,让用户点击标题切换viewpager,滑动viewpager可以切换标题
         keyboardTabLayout.setupWithViewPager(keyboardContainerViewpager);
+        //设置圆点指示器
+        keyboardFragmentAdapter.setupWithPagerPoint(keyboardContainerViewpager, roundPointGroup);
+    }
 
-        //根据页面的个数动态添加圆点指示器
-        final int count = keyboardFragmentAdapter.getCount();
-        ImageView point = null;
-        for (int i = 0; i < count; i++) {
-            // 添加指示点
-            point = new ImageView(this);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.rightMargin = 20;
-            point.setLayoutParams(params);
-            point.setBackgroundResource(R.drawable.lunbotu_round_point_selector);
-            if (i == 0) {
-                point.setEnabled(true);
-            } else {
-                point.setEnabled(false);
-            }
-            roundPointGroup.addView(point);
-        }
-
-        keyboardContainerViewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                roundPointGroup.getChildAt(position).setEnabled(true);
-                roundPointGroup.getChildAt(lastPosition).setEnabled(false); // 把上一个点设为false
-                lastPosition = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
+    private int getSoftInputHeight() {
+        Rect r = new Rect();
+        //decorView是window中的最顶层view，可以从window中通过getDecorView获取到decorView。
+        //通过decorView获取到程序显示的区域，包括标题栏，但不包括状态栏。
+        this.getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+        //获取屏幕的高度
+        int screenHeight = this.getWindow().getDecorView().getRootView().getHeight();
+        //计算软件盘的高度
+        int softInputHeight = screenHeight - r.bottom;
+        return softInputHeight;
     }
 }
--- MyTestDemo/app/src/main/java/com/haitao/www/myformer/second/ui/ui_common/dialog/popupwindow_dialog/KeyboardFragment.java
+++ MyTestDemo/app/src/main/java/com/haitao/www/myformer/second/ui/ui_common/dialog/popupwindow_dialog/KeyboardFragment.java
@@ -31,26 +31,32 @@ import java.util.List;
  * 网站：Emoji Unicode Tables，该网站上面给出了每个emoji表情的图片,描述,Unicode编码的对照表,点击表中每一项emoji就可以得到这个表情对应的Unicode，
  * 在java中的Unicode表示就是:”\ud83d\ude01”,该编码字符可以直接被Android的TextView和EditText控件识别成对应的emoji表情.
  * https://apps.timwhitlock.info/emoji/tables/unicode
- * <p>
+ *
  * 添加表情
  * https://www.2cto.com/kf/201610/556687.html
  */
 public class KeyboardFragment extends Fragment {
     private static final String Arguments = "position";
-    Activity activity;
-    TextView context_title;
-    View view;
+    private static final String Diverse = "Diverse";
+    private static final String EmojiList = "emojiList";
+    private Activity activity;
+    private GridView gridview_emoji;
+    private View view;
+    private int type;
 
     /**
      * 创建页面并传入参数
      *
      * @param position 传入的参数
+     * @param type
      * @return 为调用者返回一个fragment对象
      */
-    public static KeyboardFragment getInstance(int position, int total) {
+    public static KeyboardFragment getInstance(int position, String[] emojiList, int type) {
         KeyboardFragment fragment = new KeyboardFragment();
         Bundle args = new Bundle();
         args.putInt(Arguments, position);
+        args.putInt(Diverse, type);
+        args.putStringArray(EmojiList, emojiList);
         fragment.setArguments(args);
         return fragment;
     }
@@ -58,110 +64,29 @@ public class KeyboardFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.layout_text, container, false);
+        view = inflater.inflate(R.layout.layout_gridview_emoji, container, false);
         activity = getActivity();
-        findView(view);
-        initView();
+        initView(view);
         initData();
         return view;
     }
 
+    private void initView(View view) {
+        gridview_emoji = view.findViewById(R.id.gridview_emoji);
+        type = getArguments().getInt(Diverse);
+        switch (type) {
+            case 0:
+                gridview_emoji.setNumColumns(7);
+                break;
+            case 1:
+                gridview_emoji.setNumColumns(4);
+                break;
+        }
+    }
+
     private void initData() {
-        String[] emojis = getEmojis();
-        getPagerList(emojis);
-    }
-
-    /**
-     * 从assets目录下获取所有表情
-     *
-     * @return
-     */
-    public String[] getEmojis() {
-        BufferedReader br = null;
-        String emojis[] = null;
-        try {
-            InputStream is = activity.getAssets().open("Emoji.json");
-            StringBuffer sb = new StringBuffer();
-            br = new BufferedReader(new InputStreamReader(is));
-            String line = null;
-            while (null != (line = br.readLine())) {
-                sb.append(line).append("\r\n");
-            }
-            JSONArray emojiArray = new JSONArray(sb.toString());
-            if (null != emojiArray && emojiArray.length() > 0) {
-                emojis = new String[emojiArray.length()];
-                for (int i = 0; i < emojiArray.length(); i++) {
-                    emojis[i] = emojiArray.optString(i);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (null != br) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return emojis;
-    }
-
-    /**
-     * 获取所有表情GridView页面的集合
-     *
-     * @param emojis
-     * @return
-     */
-    public List<View> getPagerList(String[] emojis) {
-        List<View> pagers = null;
-        String[] eachPageEmojis = null;
-        if (null != emojis && emojis.length > 0) {
-            pagers = new ArrayList<>();
-            int pageCount = emojis.length / 27;//共8页表情
-            for (int i = 0; i < pageCount; i++) {
-                GridView gridView = new GridView(activity);
-                gridView.setNumColumns(7);
-                gridView.setSelector(new ColorDrawable(Color.TRANSPARENT));
-                gridView.setCacheColorHint(Color.TRANSPARENT);
-                gridView.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
-                gridView.setGravity(Gravity.CENTER);
-                eachPageEmojis = new String[28];
-                //总共216个表情字符,索引变化为:0-26,27-53,54-80,81-107,108-134,135-161,162-188,189-215
-                System.arraycopy(emojis, i * 27, eachPageEmojis, 0, 27);
-                eachPageEmojis[27] = "del";//第28是删除按钮,用特殊字符串表示
-//                gridView.setAdapter(new EmojiGvAdapter(mContext, eachPageEmojis));
-//                //点击表情监听
-//                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                    @Override
-//                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                        //获取选中的表情字符
-//                        String emoji = (String) parent.getAdapter().getItem(position);
-//                        if (null != mEmojiClickListener) {
-//                            mEmojiClickListener.onClick(emoji);
-//                        }
-//                    }
-//                });
-                pagers.add(gridView);
-            }
-        }
-        return pagers;
-    }
-
-
-    private void initView() {
-        int value = getArguments().getInt(Arguments);
-        context_title.setText("页面" + value);
-        if (value % 2 == 0) {
-            view.setBackgroundColor(getResources().getColor(R.color.red));
-        } else {
-            view.setBackgroundColor(getResources().getColor(R.color.blue));
-        }
-
-    }
-
-    private void findView(View view) {
-        context_title = view.findViewById(R.id.context_title);
+        String[] emojiArray = getArguments().getStringArray(EmojiList);
+        GridViewEmojiAdapter gridViewEmojiAdapter = new GridViewEmojiAdapter(activity, emojiArray, type);
+        gridview_emoji.setAdapter(gridViewEmojiAdapter);
     }
 }
